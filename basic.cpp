785ad56cc4d4071c9241c7ecc88559f8265ad539
Pranay Reddy <pranayreddy1024@gmail.com>
2020-06-16 13:56:04 +0530

    Reverted to previous state

--- basic.cpp
+++ basic.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 int main(void)
 {
+    vector<int> v;
+    int value;
+    cout<<"Enter elements\n";
+    while (cin>>value && value!=-72)
+    {
+        v.push_back(value);
+        /* code */
+    }
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<endl;
+    }
     
+    
+    sort(v.begin(),v.end());
+    for(auto i=v.begin();i!=v.end();++i)
+    {
+        cout<<*i<<endl;
+    }
+
+
     
     
     
